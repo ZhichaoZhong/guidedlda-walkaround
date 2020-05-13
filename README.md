47d8bf587d9040c9df6d2831fec978dfb947c8b3
ZC Zhong <>
2020-05-13 15:37:39 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Guided LDA python wrapper
+Wrapper for https://github.com/vi3k6i5/GuidedLDA
+## Installation
+```buildoutcfg
+pip install guidedlda
+```
+##Requirement
+lda: https://github.com/lda-project/lda
---
+++ setup.py
@@ -0,0 +1,16 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='guidedlda',
+    version='0.0.1',
+    url='https://github.com/ZhichaoZhong/guided-lda-wrapper',
+    author='ZhichaoZhong',
+    author_email='zzhong@wehkamp.nl',
+    description='Guided LDA wrapper',
+    install_reqs = ['lda==1.1.0'],
+    packages=["guidedlda"],
+    python_requires='>=3.7',
+)
